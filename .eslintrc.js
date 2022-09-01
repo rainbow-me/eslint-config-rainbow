eb8344f84ca305cee3758c19c5b97f39fbc1e6ee
Terry Sahaidak <tasssik@gmail.com>
2022-09-01 12:09:13 +0300

    Disable prop types

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,7 +76,7 @@ module.exports = {
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn', // TODO MUST FIX
 
     'react/jsx-props-no-spreading': 'warn',
-    'react/prop-types': 'warn', // TODO will be fixed after TS migration
+    'react/prop-types': 'off',
     'react/display-name': 'warn', // TODO easy fix
 
     // mimic default eslint
