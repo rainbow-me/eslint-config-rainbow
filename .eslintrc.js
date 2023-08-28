6e28251f7f4f17eba89dcac4d9c31c6cc7985f76
gregs <ukfaeku@gmail.com>
2023-08-28 11:18:15 -0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
   env: {
     es2022: true,
@@ -5,84 +6,87 @@ module.exports = {
     node: true,
   },
 
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
 
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
 
   extends: [
-    'eslint:recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jest/recommended',
+    "eslint:recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
 
     /**
      * For eslint-plugin-import
      *
      * @see https://github.com/import-js/eslint-plugin-import/blob/d45fe21bfa09f61402c68c3d271250d95f9c9ed3/README.md?plain=1#L156
      */
-    'plugin:import/recommended',
-    'plugin:import/typescript',
+    "plugin:import/recommended",
+    "plugin:import/typescript",
 
     /**
      * Last, must override thing
      */
-    'prettier',
+    "prettier",
   ],
 
   settings: {
-    'react': {
-      version: 'detect',
+    react: {
+      version: "detect",
     },
-    'import/resolver': {
-      'typescript': true,
-      'node': true,
-      'babel-module': true,
+    "import/resolver": {
+      typescript: true,
+      node: true,
+      "babel-module": true,
     },
   },
 
   rules: {
-    'prefer-object-spread': 'error',
-    'no-duplicate-imports': 'error',
-    'no-multi-assign': 'error',
-    'no-prototype-builtins': 'warn', // this one is probably fine
-    'no-irregular-whitespace': 'warn', // not a big deal most of the time
-    'spaced-comment': 'warn', // TODO
-    'no-var': 'warn', // TODO
-    'prefer-const': 'warn', // TODO
-    'prefer-rest-params': 'warn', // TODO
-    'prefer-spread': 'warn', // TODO
-    'no-async-promise-executor': 'warn', // TODO
-    'no-await-in-loop': 'warn', // TODO
-    'no-promise-executor-return': 'warn', // TODO
-    'require-atomic-updates': 'warn', // TODO
-    'default-param-last': 'warn', // TODO
-    'no-param-reassign': 'warn', // TODO
-    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }], // TODO
-    'no-nested-ternary': 'warn', // TODO
-    'no-unneeded-ternary': 'warn', // TODO
+    "prefer-object-spread": "error",
+    "no-duplicate-imports": "error",
+    "no-multi-assign": "error",
+    "no-prototype-builtins": "warn", // this one is probably fine
+    "no-irregular-whitespace": "warn", // not a big deal most of the time
+    "spaced-comment": "warn", // TODO
+    "no-var": "warn", // TODO
+    "prefer-const": "warn", // TODO
+    "prefer-rest-params": "warn", // TODO
+    "prefer-spread": "warn", // TODO
+    "no-async-promise-executor": "warn", // TODO
+    "no-await-in-loop": "warn", // TODO
+    "no-promise-executor-return": "warn", // TODO
+    "require-atomic-updates": "warn", // TODO
+    "default-param-last": "warn", // TODO
+    "no-param-reassign": "warn", // TODO
+    "no-plusplus": ["warn", { allowForLoopAfterthoughts: true }], // TODO
+    "no-nested-ternary": "warn", // TODO
+    "no-unneeded-ternary": "warn", // TODO
 
-    'import/no-default-export': 'warn', // TODO
+    "import/no-default-export": "warn", // TODO
 
-    '@typescript-eslint/ban-ts-comment': ['warn', { 'ts-expect-error': 'allow-with-description' }], // TODO
-    '@typescript-eslint/no-explicit-any': 'warn', // TODO
-    '@typescript-eslint/no-inferrable-types': 'warn', // TODO
-    '@typescript-eslint/no-empty-function': 'warn', // TODO
-    '@typescript-eslint/no-var-requires': 'warn', // TODO
-    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn', // TODO MUST FIX
+    "@typescript-eslint/ban-ts-comment": [
+      "warn",
+      { "ts-expect-error": "allow-with-description" },
+    ], // TODO
+    "@typescript-eslint/no-explicit-any": "warn", // TODO
+    "@typescript-eslint/no-inferrable-types": "warn", // TODO
+    "@typescript-eslint/no-empty-function": "warn", // TODO
+    "@typescript-eslint/no-var-requires": "warn", // TODO
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "warn", // TODO MUST FIX
 
-    'react/jsx-props-no-spreading': 'warn',
-    'react/prop-types': 'off',
-    'react/display-name': 'warn', // TODO easy fix
+    "react/jsx-props-no-spreading": "warn",
+    "react/prop-types": "off",
+    "react/display-name": "warn", // TODO easy fix
 
     // mimic default eslint
-    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+    "eslint-comments/disable-enable-pair": ["error", { allowWholeFile: true }],
 
     /**
      * @see https://typescript-eslint.io/docs/linting/troubleshooting/#eslint-plugin-import
@@ -94,13 +98,13 @@ module.exports = {
      * TypeScript, so we recommend that you only run them at CI/push time, to
      * lessen the local performance burden."
      */
-    'import/named': 'off',
-    'import/namespace': 'off',
-    'import/default': 'off',
-    'import/no-named-as-default-member': 'off',
-    'import/no-named-as-default': 'off', // TODO
-    'import/no-cycle': 'off', // biggest offender, but see https://github.com/rainbow-me/eslint-config-rainbow/issues/3
-    'import/no-unused-modules': 'off', // TODO
-    'import/no-deprecated': 'off', // TODO
+    "import/named": "off",
+    "import/namespace": "off",
+    "import/default": "off",
+    "import/no-named-as-default-member": "off",
+    "import/no-named-as-default": "off", // TODO
+    "import/no-cycle": "off", // biggest offender, but see https://github.com/rainbow-me/eslint-config-rainbow/issues/3
+    "import/no-unused-modules": "off", // TODO
+    "import/no-deprecated": "off", // TODO
   },
 };
